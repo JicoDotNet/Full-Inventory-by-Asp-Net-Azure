864413b0a8c066d7a31e2f74c889cb08a358708c
Soubhik Nandy <nandy.soubhik@live.co.uk>
2024-06-30 11:42:51 +0530

    sql change

--- src/JicoDotNet.SQLServer/DefaultData/Data.sql
+++ src/JicoDotNet.SQLServer/DefaultData/Data.sql
@@ -1,35 +1,35 @@
-﻿INSERT INTO dbo.mBranch(BranchName, State, IsActive, TransactionDate, RequestId)
+﻿INSERT INTO [dbo].[mBranch] (BranchName, State, IsActive, TransactionDate, RequestId)
 VALUES ('Head Branch', '29' , 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO mWareHouse (BranchId, WareHouseName, IsRetailCounter, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mWareHouse] (BranchId, WareHouseName, IsRetailCounter, IsActive, TransactionDate, RequestId)
 VALUES (CAST(SCOPE_IDENTITY() as BIGINT), 'Default Warehouse', 1, 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mUnitOfMeasure( UnitOfMeasureName, Description, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mUnitOfMeasure]( UnitOfMeasureName, Description, IsActive, TransactionDate, RequestId)
 VALUES ('Pc', 'Piece', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mProductType (ProductTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mProductType] (ProductTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Product Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mVendorType (VendorTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mVendorType] (VendorTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Vendor Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mCustomerType (CustomerTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mCustomerType] (CustomerTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Customer Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mPaymentType (PaymentTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mPaymentType] (PaymentTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Payment Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mShipmentType (ShipmentTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mShipmentType] (ShipmentTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Shipment Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mBillType (BillTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mBillType] (BillTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Bill Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mInvoiceType (InvoiceTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mInvoiceType] (InvoiceTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Invoice Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mPurchaseType (PurchaseTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mPurchaseType] (PurchaseTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Purchase Type', 1, GETDATE(), 'DefaultDataInsert');
 
-INSERT INTO dbo.mSalesType (SalesTypeName, IsActive, TransactionDate, RequestId)
+INSERT INTO [dbo].[mSalesType] (SalesTypeName, IsActive, TransactionDate, RequestId)
 VALUES ('Default Sales Type', 1, GETDATE(), 'DefaultDataInsert');
